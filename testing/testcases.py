66e45d74466a929217ccd2968e40bac6aa59ceff
EsnW <esn.w@berkeley.edu>
2024-01-15 17:21:06 -0800

    16-likes-create-api

--- testing/testcases.py
+++ testing/testcases.py
@@ -28,10 +28,12 @@ class TestCase(DjangoTestCase):
         if content is None:
             content = 'default tweet content'
         return Tweet.objects.create(user=user, content=content)
+
     def create_comment(self, user, tweet, content=None):
         if content is None:
             content = 'default comment content'
         return Comment.objects.create(user=user, tweet=tweet, content=content)
+
     def create_like(self, user, target):
         instance, _ = Like.objects.get_or_create(
             content_type=ContentType.objects.get_for_model(target.__class__),
@@ -39,3 +41,9 @@ class TestCase(DjangoTestCase):
             user=user,
         )
         return instance
+
+    def create_user_and_client(self, *args, **kwargs):
+        user = self.create_user(*args, **kwargs)
+        client = APIClient()
+        client.force_authenticate(user)
+        return user, client
